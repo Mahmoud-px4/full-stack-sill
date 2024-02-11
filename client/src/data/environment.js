a41b80fb9c47b878d7e727d35d0a41846b152e1d
Mahmoud-px3 <155615701+Mahmoud-px4@users.noreply.github.com>
2024-02-11 08:23:22 +0200

    Update environment.js

--- client/src/data/environment.js
+++ client/src/data/environment.js
@@ -2,4 +2,4 @@
 // export const website_URL = "https://tough-hen-bell-bottoms.cyclic.app"
 export const website_URL = "https://sill-clone.web.app"
 
-export const server_URL = ""
+export const server_URL = "https://zany-calf-shoulder-pads.cyclic.app"
